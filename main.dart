62de6049d20d481e80ddfc808865e80b7da87cb4
Egemberdievariet <57054923+egemberdievariet@users.noreply.github.com>
2021-11-01 15:39:40 +0600

    change

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 main(List<String> args) {
-  // print(task1(77));
-  print(task2());
+  print(task1(77));
+  print("Kalgandaryn jasai algan jok");
 }
 
 int task1(int a) {
@@ -17,18 +17,18 @@ int task1(int a) {
   return list.last;
 }
 
-String task2() {
-  int count;
-  int num1 = 50;
-  print("Is it $num1 ?");
-  for (double i = 0; i < 100; i++) {
-    if (stdin.readLineSync() == "Is greater") {
-      double num2 = num1 * 1.5;
-      print("Is it $num2 ?");
-    }else if(stdin.readLineSync() == "Is smaller"){
-      double n
-    }
+// String task2() {
+//   int count;
+//   int num1 = 50;
+//   print("Is it $num1 ?");
+//   for (double i = 0; i < 100; i++) {
+//     if (stdin.readLineSync() == "Is greater") {
+//       double num2 = num1 * 1.5;
+//       print("Is it $num2 ?");
+//     }else if(stdin.readLineSync() == "Is smaller"){
+//       double n
+//     }
 
-  }
-  return "";
-}
+//   }
+//   return "";
+// }

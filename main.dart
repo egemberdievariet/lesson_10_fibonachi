666f4dd03e5ae1d9d31f6082cdb86e8fdfd8e360
Egemberdievariet <57054923+egemberdievariet@users.noreply.github.com>
2021-11-01 15:37:10 +0600

    first commit

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+main(List<String> args) {
+  // print(task1(77));
+  print(task2());
+}
+
+int task1(int a) {
+  int n1 = 0, n2 = 1, n3;
+  List list = [];
+  for (int i = 2; i <= a; i++) {
+    n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
+    list.add(n3);
+  }
+  return list.last;
+}
+
+String task2() {
+  int count;
+  int num1 = 50;
+  print("Is it $num1 ?");
+  for (double i = 0; i < 100; i++) {
+    if (stdin.readLineSync() == "Is greater") {
+      double num2 = num1 * 1.5;
+      print("Is it $num2 ?");
+    }else if(stdin.readLineSync() == "Is smaller"){
+      double n
+    }
+
+  }
+  return "";
+}
